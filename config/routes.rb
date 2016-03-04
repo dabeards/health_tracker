93a36bc0db6a66586f76adce1cfb6cff34554d67
damianhouse <damianhouse@gmail.com>
2016-03-04 11:58:14 -0500

    Double check views folder and make some changes to headers in each file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :caloric_intakes
   resources :performed_exercises
+  resources :weights
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
