5078e6e858dd524df753a2509b91bd79ca09f7cf
damianhouse <damianhouse@gmail.com>
2016-03-06 14:49:03 -0500

    #shipit Add seeds for all classes.

--- app/models/caloric_intake.rb
+++ app/models/caloric_intake.rb
@@ -1,6 +1,8 @@
 class CaloricIntake < ActiveRecord::Base
-  
+
   def self.consumed_calories
-    group(:created_at).where(:consumed_calories).sum
+    start_time = Time.now.beginning_of_day
+    end_time = Time.now.end_of_day
+    where(:created_at).where("created_at BETWEEN ? and ?", start_time, end_time).group(:consumed_calories)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,16 @@ ExerciseType.create([
   { exercise_type_name: 'Legs' },
   { exercise_type_name: 'Abs' }
 ])
+# weights = ExerciseType.create!(exercise_type_name: "Lift Weights", created_at: "2016-03-05 21:20:00")
+# abs = ExerciseType.create!(exercise_type_name: "Ab Workout", created_at: "2016-03-04 19:19:00")
+# legs = ExerciseType.create!(exercise_type_name: "Leg Day", created_at: "2016-03-03 17:17:00")
+# yoga = ExerciseType.create!(exercise_type_name: "Hot Yoga", created_at: "2016-03-02 15:15:00")
+# basketball = ExerciseType.create!(exercise_type_name: "Basketball", created_at: "2016-03-01 13:12:11")
+# swimming = ExerciseType.create!(exercise_type_name: "Swimming", created_at: "2016-02-29 12:11:10")
+100.times do
+  # ExerciseType.create!(exercise_type_name: rand(6)+1, exercise_date: DateTime.now - (rand * 21))
+  StepCount.create!(steps_taken: rand(3000), steps_date: DateTime.now - (rand * 21))
+  PerformedExercise.create!(burned_calories: rand(900), exercise_date: DateTime.now - (rand * 21))
+  Weight.create!(weight: rand(300), weight_date: DateTime.now - (rand * 21))
+  CaloricIntake.create!(consumed_calories: rand(900), calories_date: DateTime.now - (rand * 21))
+end
