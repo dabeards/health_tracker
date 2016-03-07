d4394c798b47436f54ae6ce6429920b1ce39ef7b
damianhouse <damianhouse@gmail.com>
2016-03-06 21:36:17 -0500

    #shipit Add method total steps taken today and add them to dashboard.

--- app/models/step_count.rb
+++ app/models/step_count.rb
@@ -1,2 +1,8 @@
 class StepCount < ActiveRecord::Base
+
+  def self.total_steps_taken_today
+    if where(['created_at > ?', DateTime.now.beginning_of_day]).exists?
+      where(['created_at > ?', DateTime.now.beginning_of_day]).sum(:steps_taken)
+    end
+  end
 end
