6266bed6579a0b1bd37f6cefd76494e89a326305
damianhouse <damianhouse@gmail.com>
2016-03-06 21:57:50 -0500

    #shipit Add weight last entered method and add statistic to dashboard.

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,3 +1,9 @@
 class Weight < ActiveRecord::Base
   validates :weight, uniqueness: { scope: :weight_date }
+
+  def self.last_weight_entered
+    if where(['created_at > ?', DateTime.now.beginning_of_day]).exists?
+      where(['created_at > ?', DateTime.now.beginning_of_day]).sum(:weight)
+    end
+  end
 end
